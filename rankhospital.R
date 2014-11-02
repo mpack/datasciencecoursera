a3d701410e9851dcaf7806cec34da81ca4584e62
mpack <mp94564@gmail.com>
2014-11-02 10:17:03 -0800

    initial rankhospital.R commit.

---
+++ rankhospital.R
@@ -0,0 +1,7 @@
+rankhospital <- function(state, outcome, num = "best") {
+    ## Read oucome data
+    
+    ## Check that state and outcome are valid
+    
+    ## return return hospital name in state with given rank
+}
